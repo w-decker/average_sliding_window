9b276a5d51f5cfe96b7fd79fd0b01386a4caa4fe
Chris Cox <chriscox@lsu.edu>
2023-06-29 16:26:40 -0500

    Fixed some logic * Zero-based indexing logic * A mis-reading of equation 1 from paper --> Each element is multiplied, and the product of means subtracted. --> Then sum over the differences.

--- swc_module.py
+++ swc_module.py
@@ -9,8 +9,8 @@ def swc_loop(x, y, delta, TR):
     r = np.zeros((len(x)))
 
     for n in range(delta, (len(x) - delta)):
-        a = n - delta + 1
+        a = n - delta
         b = n + delta
-        r[n] = (TR / h) * (np.sum(np.dot(x[a:b], y[a:b])) - np.mean(x[a:b]) * np.mean(y[a:b]))
+        r[n] = (TR / h) * np.sum((x[a:b] * y[a:b]) - (np.mean(x[a:b]) * np.mean(y[a:b])))
 
     return r, float(h)
