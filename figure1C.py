d675dcb2348639fdc9472d0adceba78bcd6f366f
Your Name <deckerwill7@gmail.com>
2023-06-30 09:37:59 -0500

    corrected figure1C.py error * error found in np.count_nonzero...axis was not previously specified

--- figure1C.py
+++ figure1C.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from swc_module import swc_loop
 
 
 Hz = 0.025
@@ -7,24 +8,32 @@ nt = 120
 w = 2 * np.pi * Hz
 k = np.arange(nt)
 TR = 1.0
+theta = np.arccos(.2); # phase difference
+
+x = np.cos(w * k * TR)
+x = x / np.std(x)
+y = np.cos(w * k * TR  + theta)
+y = y / np.std(y)
+
 r = np.zeros((60, nt))
 h = np.zeros((60, 1))
+
+for delta in range(60):
+    r[delta, :], h[delta] = swc_loop(x, y, delta+1, TR)
+
+h = np.tile(h, (1, nt))
+z = r != 0
 r1 = r[24, :]
 z = r1 != 0
 G = [None] * np.count_nonzero(z)
 
-for g in range(np.count_nonzero(z)):
+for g in range(np.count_nonzero(z, axis=0)):
     G[g] = np.convolve(r1[z], np.ones(g+1) / (g+1), mode='valid')
 
 nc = [len(g) for g in G]
-g = np.repeat(np.arange(1, np.count_nonzero(z)+1), nc)
+g = np.repeat(np.arange(1, np.count_nonzero(z, axis=0)+1), nc)
 
-# TO DO: - Figure out why np.concatenate works in jupyter and not plain python file
-
-GG = np.concatenate(G) # ERROR OCCURING HERE
-
-
-plt.scatter(g, GG, s=1, marker='.')
+plt.scatter(g, np.concatenate(G), s=1, marker='.')
 plt.xlim((0, 120))
 plt.ylim((-0.1, 0.4))
 plt.hlines(y=0.2, xmin=0, xmax=120, colors='red')
